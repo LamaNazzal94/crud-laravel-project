493a96663adcd18b8b58ab202c48196a46209943
malek96sawalha <maleksawalha1996@gmail.com>
2023-09-05 12:19:42 +0300

    m

--- app/Models/Studant.php
+++ app/Models/Studant.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Studant extends Model
 {
     use HasFactory;
+    protected $table = "studants";
+    protected $fillable = [
+       'fname','lname','email','age'
+    ];
 }
